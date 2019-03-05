ab81d87a1012e44d0432aae45b71cc0e97a8ae95
Desmond Edem <kukiito219@gmail.com>
2019-03-05 04:31:30 +0100

    add tests

--- src/server.js
+++ src/server.js
@@ -10,7 +10,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/v1', router);
 
 app.get('/', (req, res) => res.status(200).send({ message: 'Welcome to Reflections!' }));
+app.all('*', (req, res) => res.status(404).send({ message: 'Wrong endpoint. Endpoint does not exist' }));
 
 app.listen(PORT, () => {
   console.log(`Server running at ${PORT}`);
 });
+
+export default app;
